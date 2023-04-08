f79ff5ae63627a18ac01a46b43d88e0108185b8d
VitalyMyalkin <myalych@mail.ru>
2023-04-09 00:53:36 +0300

    first increment

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,3 +1,42 @@
 package main
 
-func main() {}
+import (
+	"net/http"
+)
+
+func postHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Только Post запросы!!", http.StatusBadRequest)
+		return
+	}
+	body := "URL"
+	w.Header().Set("content-type", "text/plain")
+	// устанавливаем код 201
+	w.WriteHeader(http.StatusCreated)
+	// пишем тело ответа
+	w.Write([]byte(body))
+}
+
+func getHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		http.Error(w, "Только Get запросы!!", http.StatusBadRequest)
+		return
+	}
+
+	w.Header().Set("Location", "URL")
+	// устанавливаем код 307
+	w.WriteHeader(http.StatusTemporaryRedirect)
+	// пишем тело ответа
+	w.Write([]byte(""))
+}
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc(`/`, postHandler)
+	mux.HandleFunc(`/{id}`, getHandler)
+
+	err := http.ListenAndServe(`:8080`, mux)
+	if err != nil {
+		panic(err)
+	}
+}
